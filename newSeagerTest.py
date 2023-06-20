595effcf9c1b97b02eea3ec724e48cad09c3a2dc
= <=>
2023-06-20 14:19:24 +0100

    Removed uneccessary files

--- newSeagerTest.py
+++
@@ -1,9 +0,0 @@
-from localisation import *
-import networkx as nx
-
-
-tree = nx.read_adjlist("treeFiles\\randomTree100.txt")
-
-game = Localisation(tree, TargetMovement.tProbabilistic, ProbeMovement.seager)
-game.play()
-print(game.win)
